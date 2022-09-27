2f1a7d4025dc18ffca09bbd9c886a2e69f19a02c
henni saaranen <n0sahe00@students.oamk.fi>
2022-09-27 19:00:32 +0300

    https://github.com/Henspa/Alert.git

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 
 export default function App() {
+
+  const [ok, setOk] = useState(false);
+ 
+  const showAlert = () => {
+    Alert.alert(
+      "My title",
+      "This is my message.",
+      [
+        {
+          text: "Ok",
+          onPress: () => setOk(true)
+        },
+        {
+          text: "Cancel",
+          onPress: () => setOk(false)
+        }
+      ]
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title={'Open dialog'} onPress={showAlert}></Button>
+      <Text>{ok===true ? 'ok' : 'cancel'}</Text>
     </View>
   );
 }
